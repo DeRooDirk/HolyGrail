5df236033b6a688e1b984321f4e6b301bf1aaede
Zoo Wie <de-roo.dirk@hotmail.com>
2020-10-23 16:43:52 +0200

    index
    fun update

--- zoo.js
+++ zoo.js
@@ -1,16 +1,4 @@
 
-/* flashlight --*/
-
-document.addEventListener("mousemove", function (e) {
-    var gradient = document.querySelector(".gradient");
-    gradient.style.background = "none";
-    gradient.style.backgroundImage = "radial-gradient(circle closest-corner at " + e.clientX + "px " + e.clientY + "px , rgba(0,0,0,0) 0%, rgba(0,0,0,.9) 20%)";
-});
-
-document.addEventListener("mouseout", function (e) {
-    var gradient = document.querySelector(".gradient");
-    gradient.style.background = "rgba(0,0,0,.9)";
-});
 
 /* Background animatie --*/
 $('.dirk').mousemove(function(e){
